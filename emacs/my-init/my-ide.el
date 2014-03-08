9d7f0b7bf00d1e364bed9ba600f3fe104871033f
Chunliang Lyu <hi@chunlianglyu.com>
2014-03-08 13:32:27 +0800

    add new paths, fix auctex in emacs.

--- emacs/my-init/my-ide.el
+++ emacs/my-init/my-ide.el
@@ -65,12 +65,12 @@
 
 ;; Auctex
 ; manual install
-(add-to-list 'load-path "~/.emacs.d/package/auctex")
-(add-to-list 'load-path "~/.emacs.d/package/auctex/preview")
-(load "auctex.el" nil t t)
-(load "preview-latex.el" nil t t)
-(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-(setq LaTeX-command "pdflatex")
+;(add-to-list 'load-path "~/.emacs.d/package/auctex")
+;(add-to-list 'load-path "~/.emacs.d/package/auctex/preview")
+;(load "auctex.el" nil t t)
+;(load "preview-latex.el" nil t t)
+;(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+;(setq LaTeX-command "pdflatex")
 ; spell check
 (add-hook 'LaTex-mode-hook 'flyspell-mode)
 (add-hook 'LaTex-mode-hook 'flyspell-buffer)
