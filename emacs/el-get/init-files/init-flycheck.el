7fe628aa2634e421078398b062d339ff357f847c
Chunliang Lyu <hi@chunlianglyu.com>
2014-06-09 13:11:49 +0800

    only check syntax for python and js, add android path.

--- emacs/el-get/init-files/init-flycheck.el
+++ emacs/el-get/init-files/init-flycheck.el
@@ -1,1 +1,3 @@
-(add-hook 'after-init-hook #'global-flycheck-mode)
+;;(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'python-mode-hook 'flycheck-mode)
+(add-hook 'js2-mode-hook 'flycheck-mode)
